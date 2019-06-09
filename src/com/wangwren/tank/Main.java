f3abcea7794370b4597c5812e3cb9373a48d0d90
wangwren <291762380@qq.com>
2019-06-09 17:47:42 +0800

    去掉敌人坦克，为网络版做准备

--- src/com/wangwren/tank/Main.java
+++ src/com/wangwren/tank/Main.java
@@ -6,12 +6,12 @@ public class Main {
 		TankFrame f = new TankFrame();	
 		
 		//敌军坦克数量
-		int initTankCount = Integer.parseInt((String)PropertyMgr.getProp("initTankCount"));
-		
-		//创建五个敌军坦克
-		for(int i = 0 ; i < initTankCount ; i ++) {
-			f.tanks.add(new Tank(100 + i * 80,300,Dir.DOWN,Group.BAD,f));
-		}
+//		int initTankCount = Integer.parseInt((String)PropertyMgr.getProp("initTankCount"));
+//		
+//		//创建五个敌军坦克
+//		for(int i = 0 ; i < initTankCount ; i ++) {
+//			f.tanks.add(new Tank(100 + i * 80,300,Dir.DOWN,Group.BAD,f));
+//		}
 		
 //		for(int i = 0 ; i < 10 ; i ++) {
 //			new Thread(() -> {
